c641b684a48b7f9386b543499a101bbcbeb57946
AlexanderMcMarshall <bujorsandu22@gmail.com>
2026-03-15 01:43:34 +0200

    location fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    base: "/AlexanderMcMarshall/",
+    base: "/artemis-rp-romania/",
     port: 8080,
     hmr: {
       overlay: false,
